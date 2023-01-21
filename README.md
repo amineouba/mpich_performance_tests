295ef51daf7395b1b94680b73a62ee03404d0a6e
amineouba <63561311+amineouba@users.noreply.github.com>
2023-01-21 04:53:48 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+To compare the old MPICH and the MPCIH with optimized collectives. You need to download the two versions.
+
+* The old version (MPCIH-1.2.0) : https://archive.org/download/tucows_8930_MPICH
+
+* The newer (MPCIH2-1.0) : https://www.mpich.org/static/downloads/1.0/
+
+* In the two versions, there is a folder "doc/" with a document (install.pdf,  install.ps.gz) containing an installation guide.
+
+As I wasn't able to install these versions on Grid'5000 (no guide for MPICH), I ran the tests on my machine with 4 processes.
+
+There is a file for each collective operation in which :
+for different message sizes, I run 10 tests and calculate the average time.
+
+Instead of using MPI_Wtime() function to measure the execution time, I used clock() because it has high-resolution timer.
---
+++ reduce.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <mpi.h>
+#include <stdlib.h>
+#include <time.h>
+
+const int NUM_OF_MSGS = 8;
+const int NUM_TESTS = 10;
+
+int main(int argc, char** argv) {
+    int rank, size;
+    int* send_data;
+    int* recv_data;
+    int message_size;
+    double execution_time;
+    int message_sizes[8] = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000};
+    double* execution_times;
+    clock_t start_t, end_t;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    execution_times = (double*)malloc(NUM_OF_MSGS * sizeof(double));
+
+    for (int i = 0; i < NUM_OF_MSGS; i++) {
+        message_size = message_sizes[i];
+        execution_time = 0;
+        for (int j = 0; j < NUM_TESTS; j++) {   
+            send_data = (int*)malloc(message_size * sizeof(int));
+            recv_data = (int*)malloc(message_size * sizeof(int));
+
+            MPI_Barrier(MPI_COMM_WORLD);
+            start_t = clock();
+            MPI_Reduce(send_data, recv_data, message_size, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+            MPI_Barrier(MPI_COMM_WORLD);
+            end_t = clock();
+
+            execution_time += (double)(end_t - start_t) * 1000 / CLOCKS_PER_SEC;
+            free(send_data);
+            free(recv_data);
+        }
+        execution_times[i] = execution_time / NUM_TESTS;
+    }
+
+    if (rank == 0) {
+        printf("{ ");
+        for (int i = 0; i < NUM_OF_MSGS; i++) {
+            printf("%d: %f, ", message_sizes[i], execution_times[i]);
+        }
+        printf("}\n");
+    }
+
+    free(execution_times);
+    MPI_Finalize();
+    return 0;
+}
